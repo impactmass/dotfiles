65c2040f9b725dc93e11e06eda682204b9468130
Seun Martins <seundotme@gmail.com>
2020-01-27 08:17:12 +0000

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,13 +10,13 @@ function doIt() {
     --exclude "README.md" \
     --exclude "LICENSE-MIT.txt" \
     -avh --no-perms . ~;
-  source ~/.zshrc;
+  source ~/.bashrc;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
   doIt;
 else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+  read -r -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
   echo "";
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     doIt;
